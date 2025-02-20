ea96496aa252219d72ef18d55c77dedd6aeb6b09
tiago souza <tsouza@CASSIND.SE>
2025-02-20 13:32:31 -0300

    Mesclador PDF

---
+++ app.py
@@ -0,0 +1,14 @@
+import PyPDF2 
+import os
+
+ferramenta = PyPDF2.PdfMerger()
+
+lista_arquivos = os.listdir('arquivos')
+lista_arquivos.sort()
+print(lista_arquivos)
+
+for arquivo in lista_arquivos:
+    if ".pdf":
+        ferramenta.append(f"arquivos/{arquivo}")
+
+ferramenta.write("PDF Final.pdf")
